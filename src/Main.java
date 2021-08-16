11482174aae92558b2a4fd370b166b13ff92f414
StaroePovidlo <gryaznownikolay@gmail.con>
2021-08-16 10:14:39 +0300

    release v2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] task = in.nextLine().split(" ");
+        StringBuilder postfix = new StringBuilder();
+        for (int i = 0; i < task[1].length();) {
+            char c = task[1].charAt(i);
+            if(task[0].indexOf(c) != -1) {
+                task[0] = task[0].replaceFirst(""+c,"");
+                task[1] = task[1].replaceFirst(""+c,"");
+            } else {
+                postfix.append(c);
+                i++;
+            }
+        }
+        task[0] += postfix;
+        System.out.println(task[0]);
+    }
 }
